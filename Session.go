b0ae9ea035cdead27dd80989af5c0d6d0317783d
Eduard Urbach <e.urbach@gmail.com>
2017-06-08 23:27:31 +0200

    Improved aerospike session store

--- Session.go
+++ Session.go
@@ -1,26 +1,31 @@
 package arn
 
-import as "github.com/aerospike/aerospike-client-go"
-import "github.com/aerogo/aero"
+import (
+	"fmt"
+
+	"github.com/aerogo/aero"
+	as "github.com/aerospike/aerospike-client-go"
+)
 
 // SessionStoreAerospike is a store saving sessions in an Aerospike database.
 type SessionStoreAerospike struct {
-	namespace string
+	set string
 }
 
 // NewAerospikeStore creates a session store using an Aerospike database.
-func NewAerospikeStore() *SessionStoreAerospike {
+func NewAerospikeStore(set string) *SessionStoreAerospike {
 	return &SessionStoreAerospike{
-		namespace: "Session",
+		set: set,
 	}
 }
 
 // Get loads the initial session values from the database.
 func (store *SessionStoreAerospike) Get(sid string) *aero.Session {
-	key, _ := as.NewKey(namespace, store.namespace, sid)
+	key, _ := as.NewKey(namespace, store.set, sid)
 	record, err := client.Get(nil, key)
 
-	if err != nil {
+	if err != nil || record == nil {
+		fmt.Println(err)
 		return nil
 	}
 
@@ -30,11 +35,11 @@ func (store *SessionStoreAerospike) Get(sid string) *aero.Session {
 // Set updates the session values in the database.
 func (store *SessionStoreAerospike) Set(sid string, session *aero.Session) {
 	sessionData := session.Data()
-	key, _ := as.NewKey(namespace, store.namespace, sid)
+	key, _ := as.NewKey(namespace, store.set, sid)
 
-	if len(sessionData) == 0 {
-		go client.Delete(nil, key)
+	if sessionData == nil {
+		client.Delete(nil, key)
 	} else {
-		go client.PutObject(nil, key, sessionData)
+		client.Put(nil, key, sessionData)
 	}
 }
