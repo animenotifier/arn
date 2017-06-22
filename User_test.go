0e694536025d7bc5dc15ebe9cc7d91c9d00cc4ed
Eduard Urbach <e.urbach@gmail.com>
2017-06-22 15:47:18 +0200

    Added DB error message test

--- User_test.go
+++ User_test.go
@@ -1,6 +1,7 @@
 package arn
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -14,4 +15,13 @@ func TestNewUser(t *testing.T) {
 	}
 
 	assert.NotEmpty(t, user.ID)
+}
+
+func TestDatabaseErrorMessages(t *testing.T) {
+	_, err := GetUser("NON EXISTENT USER ID")
+
+	// We need to make sure that non-existent records return "not found" in the error message.
+	assert.NotNil(t, err)
+	assert.NotEmpty(t, err.Error())
+	assert.NotEqual(t, -1, strings.Index(err.Error(), "not found"))
 }
