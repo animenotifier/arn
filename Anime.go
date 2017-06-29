7df5ce61530ff487001101dc4194958582b517fa
Eduard Urbach <e.urbach@gmail.com>
2017-06-29 06:35:55 +0200

    Added hiragana

--- Anime.go
+++ Anime.go
@@ -63,6 +63,7 @@ type AnimeTitle struct {
 	Romaji    string   `json:"romaji"`
 	English   string   `json:"english"`
 	Japanese  string   `json:"japanese"`
+	Hiragana  string   `json:"hiragana"`
 	Canonical string   `json:"canonical"`
 	Synonyms  []string `json:"synonyms"`
 }
