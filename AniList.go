58eb88662eead274fc105dff66eecff3b85f7322
Eduard Urbach <e.urbach@gmail.com>
2017-07-10 01:10:17 +0200

    Cleanup

--- AniList.go
+++ AniList.go
@@ -66,3 +66,21 @@ func FindAniListAnime(search *anilist.Anime, allAnime []*Anime) *Anime {
 
 	return nil
 }
+
+// AniListAnimeListStatus returns the ARN version of the anime status.
+func AniListAnimeListStatus(item *anilist.AnimeListItem) string {
+	switch item.ListStatus {
+	case "watching":
+		return AnimeListStatusWatching
+	case "completed":
+		return AnimeListStatusCompleted
+	case "plan to watch":
+		return AnimeListStatusPlanned
+	case "on-hold":
+		return AnimeListStatusHold
+	case "dropped":
+		return AnimeListStatusDropped
+	default:
+		return AnimeListStatusPlanned
+	}
+}
