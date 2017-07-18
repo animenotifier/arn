f17a08a39cf08313188d744c7316cc40fe6d1b19
Eduard Urbach <e.urbach@gmail.com>
2017-07-18 07:56:26 +0200

    Avatar refresh

--- SettingsAPI.go
+++ SettingsAPI.go
@@ -1,6 +1,8 @@
 package arn
 
 import (
+	"reflect"
+
 	"github.com/aerogo/aero"
 )
 
@@ -12,7 +14,24 @@ func (settings *Settings) Authorize(ctx *aero.Context) error {
 // Update updates the settings object.
 func (settings *Settings) Update(ctx *aero.Context, data interface{}) error {
 	updates := data.(map[string]interface{})
-	return SetObjectProperties(settings, updates, nil)
+	return SetObjectProperties(settings, updates, func(fullKeyName string, field *reflect.StructField, property *reflect.Value, newValue reflect.Value) (bool, error) {
+		switch fullKeyName {
+		case "Avatar.Source":
+			settings.Avatar.Source = newValue.String()
+			settings.Save() // Save needed here because RefreshAvatar fetches the settings on another server
+			settings.User().RefreshAvatar()
+			return true, nil
+
+		case "Avatar.SourceURL":
+			settings.Avatar.SourceURL = newValue.String()
+			settings.Save() // Save needed here because RefreshAvatar fetches the settings on another server
+			settings.User().RefreshAvatar()
+			return true, nil
+
+		default:
+			return false, nil
+		}
+	})
 }
 
 // Save saves the settings in the database.
