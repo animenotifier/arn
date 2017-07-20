a5e019563c749be041631d105eec3495ad75701b
Eduard Urbach <e.urbach@gmail.com>
2017-07-20 14:27:50 +0200

    Improved search

--- SearchIndex.go
+++ SearchIndex.go
@@ -34,24 +34,24 @@ func GetSearchIndex(id string) (*SearchIndex, error) {
 }
 
 // Search is a fuzzy search.
-func Search(term string, maxUsers, maxAnime int) ([]*User, []*Anime) {
+func Search(term string, maxUsers, maxAnime, maxPosts, maxThreads int) ([]*User, []*Anime, []*Post, []*Thread) {
 	term = strings.ToLower(term)
-	term = RemoveSpecialCharacters(term)
 
 	if term == "" {
-		return nil, nil
+		return nil, nil, nil, nil
 	}
 
 	var userResults []*User
 	var animeResults []*Anime
+	var postResults []*Post
+	var threadResults []*Thread
 
 	type SearchItem struct {
 		text       string
 		similarity float64
 	}
 
-	// Search everything in parallel
-	flow.Parallel(func() {
+	searchUsers := func() {
 		// Search userResults
 		var user *User
 
@@ -103,7 +103,12 @@ func Search(term string, maxUsers, maxAnime int) ([]*User, []*Anime) {
 
 			userResults = append(userResults, user)
 		}
-	}, func() {
+	}
+
+	searchAnime := func() {
+		// Remove special characters when searching anime titles
+		animeSearchTerm := RemoveSpecialCharacters(term)
+
 		// Search anime
 		var anime *Anime
 
@@ -121,9 +126,9 @@ func Search(term string, maxUsers, maxAnime int) ([]*User, []*Anime) {
 
 		for name, id := range textToID {
 			cleanName := RemoveSpecialCharacters(name)
-			s := StringSimilarity(term, cleanName)
+			s := StringSimilarity(animeSearchTerm, cleanName)
 
-			if strings.Contains(cleanName, term) {
+			if strings.Contains(cleanName, animeSearchTerm) {
 				s += 0.5
 			}
 
@@ -172,7 +177,85 @@ func Search(term string, maxUsers, maxAnime int) ([]*User, []*Anime) {
 
 			animeResults = append(animeResults, anime)
 		}
-	})
+	}
 
-	return userResults, animeResults
+	searchPosts := func() {
+		postSearchIndex, err := GetSearchIndex("Post")
+
+		if err != nil {
+			return
+		}
+
+		textToID := postSearchIndex.TextToID
+
+		// Search items
+		items := make([]string, 0)
+
+		for text, postID := range textToID {
+			if !strings.Contains(text, term) {
+				continue
+			}
+
+			items = append(items, postID)
+
+			// Limit
+			if len(items) >= maxPosts {
+				break
+			}
+		}
+
+		// Fetch data
+		obj, err := DB.GetMany("Post", items)
+
+		if err != nil {
+			return
+		}
+
+		postResults = obj.([]*Post)
+	}
+
+	searchThreads := func() {
+		threadSearchIndex, err := GetSearchIndex("Thread")
+
+		if err != nil {
+			return
+		}
+
+		textToID := threadSearchIndex.TextToID
+
+		// Search items
+		items := make([]string, 0)
+
+		for text, threadID := range textToID {
+			if !strings.Contains(text, term) {
+				continue
+			}
+
+			items = append(items, threadID)
+
+			// Limit
+			if len(items) >= maxThreads {
+				break
+			}
+		}
+
+		// Fetch data
+		obj, err := DB.GetMany("Thread", items)
+
+		if err != nil {
+			return
+		}
+
+		threadResults = obj.([]*Thread)
+	}
+
+	// Search everything in parallel
+	flow.Parallel(
+		searchUsers,
+		searchAnime,
+		searchPosts,
+		searchThreads,
+	)
+
+	return userResults, animeResults, postResults, threadResults
 }
