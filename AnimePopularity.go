88d18906bfab3c2508428a8b6980a15f6443a7b9
Eduard Urbach <e.urbach@gmail.com>
2017-09-30 16:03:40 +0200

    Added total

--- AnimePopularity.go
+++ AnimePopularity.go
@@ -8,3 +8,8 @@ type AnimePopularity struct {
 	Hold      int `json:"hold"`
 	Dropped   int `json:"dropped"`
 }
+
+// Total returns the total number of users that added this anime to their collection.
+func (p *AnimePopularity) Total() int {
+	return p.Watching + p.Completed + p.Planned + p.Hold + p.Dropped
+}
