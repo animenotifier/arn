55135bfeb4383f4186403558bee98e758f92d593
Eduard Urbach <e.urbach@gmail.com>
2017-11-18 11:22:45 +0100

    Added streaming utils for Company

--- Company.go
+++ Company.go
@@ -1,5 +1,7 @@
 package arn
 
+import "github.com/aerogo/nano"
+
 // Company ...
 type Company struct {
 	ID          string      `json:"id"`
@@ -12,4 +14,43 @@ type Company struct {
 	CreatedBy   string      `json:"createdBy"`
 	Edited      string      `json:"edited"`
 	EditedBy    string      `json:"editedBy"`
+}
+
+// GetCompany returns a single company.
+func GetCompany(id string) (*Company, error) {
+	obj, err := DB.Get("Company", id)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return obj.(*Company), nil
+}
+
+// StreamCompanies returns a stream of all companies.
+func StreamCompanies() chan *Company {
+	channel := make(chan *Company, nano.ChannelBufferSize)
+
+	go func() {
+		for obj := range DB.All("Company") {
+			channel <- obj.(*Company)
+		}
+
+		close(channel)
+	}()
+
+	return channel
+}
+
+// AllCompanies returns a slice of all companies.
+func AllCompanies() []*Company {
+	var all []*Company
+
+	stream := StreamCompanies()
+
+	for obj := range stream {
+		all = append(all, obj)
+	}
+
+	return all
 }
