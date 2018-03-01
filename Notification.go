c29d2d5e0679f2dd3f75a4e2d2439bc1fcea3d0a
Eduard Urbach <e.urbach@gmail.com>
2018-03-01 23:18:58 +0100

    Added notification utilities

--- Notification.go
+++ Notification.go
@@ -1,5 +1,7 @@
 package arn
 
+import "github.com/aerogo/nano"
+
 // Notification represents a user-associated notification.
 type Notification struct {
 	ID      string `json:"id"`
@@ -7,6 +9,12 @@ type Notification struct {
 	Created string `json:"created"`
 	Seen    string `json:"seen"`
 	PushNotification
+}
+
+// User retrieves the user the notification was sent to.
+func (notification *Notification) User() *User {
+	user, _ := GetUser(notification.UserID)
+	return user
 }
 
 // NewNotification creates a new notification.
@@ -18,4 +26,41 @@ func NewNotification(userID string, pushNotification *PushNotification) *Notific
 		Seen:             "",
 		PushNotification: *pushNotification,
 	}
+}
+
+// GetNotification ...
+func GetNotification(id string) (*Notification, error) {
+	obj, err := DB.Get("Notification", id)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return obj.(*Notification), nil
+}
+
+// StreamNotifications returns a stream of all notifications.
+func StreamNotifications() chan *Notification {
+	channel := make(chan *Notification, nano.ChannelBufferSize)
+
+	go func() {
+		for obj := range DB.All("Notification") {
+			channel <- obj.(*Notification)
+		}
+
+		close(channel)
+	}()
+
+	return channel
+}
+
+// AllNotifications returns a slice of all notifications.
+func AllNotifications() ([]*Notification, error) {
+	var all []*Notification
+
+	for obj := range StreamNotifications() {
+		all = append(all, obj)
+	}
+
+	return all, nil
 }
