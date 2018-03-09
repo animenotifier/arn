92bf0db905324e18f37a8f98bca4e901a56551e4
Eduard Urbach <e.urbach@gmail.com>
2018-03-09 13:42:32 +0100

    Implemented diff ignore API

--- IgnoreAnimeDifference.go
+++ IgnoreAnimeDifference.go
@@ -1,11 +1,40 @@
 package arn
 
+import "fmt"
+
 // IgnoreAnimeDifference saves which differences between anime providers can be ignored.
 type IgnoreAnimeDifference struct {
 	// The ID is built like this: arn:323|mal:356|JapaneseTitle
 	ID        string `json:"id"`
-	Note      string `json:"note"`
 	ValueHash uint64 `json:"valueHash"`
 	Created   string `json:"created"`
 	CreatedBy string `json:"createdBy"`
+}
+
+// GetIgnoreAnimeDifference ...
+func GetIgnoreAnimeDifference(id string) (*IgnoreAnimeDifference, error) {
+	obj, err := DB.Get("IgnoreAnimeDifference", id)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return obj.(*IgnoreAnimeDifference), nil
+}
+
+// CreateDifferenceID ...
+func CreateDifferenceID(animeID string, dataProvider string, malAnimeID string, typeName string) string {
+	return fmt.Sprintf("arn:%s|%s:%s|%s", animeID, dataProvider, malAnimeID, typeName)
+}
+
+// IsAnimeDifferenceIgnored tells you whether the given difference is being ignored.
+func IsAnimeDifferenceIgnored(animeID string, dataProvider string, malAnimeID string, typeName string, hash uint64) bool {
+	key := CreateDifferenceID(animeID, dataProvider, malAnimeID, typeName)
+	ignore, err := GetIgnoreAnimeDifference(key)
+
+	if err != nil {
+		return false
+	}
+
+	return ignore.ValueHash == hash
 }
