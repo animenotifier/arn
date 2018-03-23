b117b626f7aa679e5b8a3fe66421300793da906f
Eduard Urbach <e.urbach@gmail.com>
2018-03-23 19:03:53 +0100

    Added GetCompanyToAnimeMap

--- CompanySort.go
+++ CompanySort.go
@@ -4,6 +4,22 @@ import (
 	"sort"
 )
 
+// GetCompanyToAnimeMap returns a map that contains company IDs as keys and their anime as values.
+func GetCompanyToAnimeMap() map[string][]*Anime {
+	companyToAnimes := map[string][]*Anime{}
+
+	allAnime := AllAnime()
+	SortAnimeByQuality(allAnime)
+
+	for _, anime := range allAnime {
+		for _, studioID := range anime.StudioIDs {
+			companyToAnimes[studioID] = append(companyToAnimes[studioID], anime)
+		}
+	}
+
+	return companyToAnimes
+}
+
 // SortCompaniesPopularFirst ...
 func SortCompaniesPopularFirst(companies []*Company) {
 	// Generate company ID to popularity map
