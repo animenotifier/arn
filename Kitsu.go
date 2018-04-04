0e8ec56a41e81d9eefc8a854762de8c7c489d98f
Eduard Urbach <e.urbach@gmail.com>
2018-04-04 15:08:13 +0200

    Use Kitsu finder

--- Kitsu.go
+++ Kitsu.go
@@ -1,12 +1,28 @@
 package arn
 
-// FindKitsuAnime tries to find a Kitsu anime in our anime database.
-func FindKitsuAnime(searchID string, allAnime []*Anime) *Anime {
-	for _, anime := range allAnime {
-		if anime.GetMapping("kitsu/anime") == searchID {
-			return anime
+// KitsuFinder ...
+type KitsuFinder struct {
+	kitsuIDToAnime map[string]*Anime
+}
+
+// NewKitsuFinder creates a new finder for Kitsu anime.
+func NewKitsuFinder() *KitsuFinder {
+	finder := &KitsuFinder{
+		kitsuIDToAnime: map[string]*Anime{},
+	}
+
+	for anime := range StreamAnime() {
+		kitsuID := anime.GetMapping("kitsu/anime")
+
+		if kitsuID != "" {
+			finder.kitsuIDToAnime[kitsuID] = anime
 		}
 	}
 
-	return nil
+	return finder
+}
+
+// GetAnime tries to find a Kitsu anime in our anime database.
+func (finder *KitsuFinder) GetAnime(kitsuID string) *Anime {
+	return finder.kitsuIDToAnime[kitsuID]
 }
