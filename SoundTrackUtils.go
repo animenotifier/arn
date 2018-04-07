f03feddee8113503ee5f7156e22fd0768aaf4c0f
Eduard Urbach <e.urbach@gmail.com>
2018-04-07 15:29:34 +0200

    Removed GetSoundCloudMedia

--- SoundTrackUtils.go
+++ SoundTrackUtils.go
@@ -1,43 +1,11 @@
 package arn
 
 import (
-	"encoding/json"
 	"errors"
 	"regexp"
-	"strconv"
-
-	"github.com/parnurzeal/gorequest"
 )
 
 var youtubeIDRegex = regexp.MustCompile(`youtu(?:.*\/v\/|.*v=|\.be\/)([A-Za-z0-9_-]{11})`)
-
-// GetSoundCloudMedia returns an ExternalMedia object for the given Soundcloud link.
-func GetSoundCloudMedia(url string) (*ExternalMedia, error) {
-	var err error
-	_, body, errs := gorequest.New().Get("https://api.soundcloud.com/resolve.json?url=" + url + "&client_id=" + APIKeys.SoundCloud.ID).EndBytes()
-
-	if len(errs) > 0 {
-		return nil, errs[0]
-	}
-
-	var soundcloud SoundCloudTrack
-	err = json.Unmarshal(body, &soundcloud)
-
-	if err != nil {
-		return nil, err
-	}
-
-	if soundcloud.ID == 0 {
-		return nil, errors.New("Invalid Soundcloud response as the ID is not valid")
-	}
-
-	soundCloudID := strconv.Itoa(soundcloud.ID)
-
-	return &ExternalMedia{
-		Service:   "SoundCloud",
-		ServiceID: soundCloudID,
-	}, nil
-}
 
 // GetYoutubeMedia returns an ExternalMedia object for the given Youtube link.
 func GetYoutubeMedia(url string) (*ExternalMedia, error) {
@@ -56,3 +24,31 @@ func GetYoutubeMedia(url string) (*ExternalMedia, error) {
 
 	return media, nil
 }
+
+// // GetSoundCloudMedia returns an ExternalMedia object for the given Soundcloud link.
+// func GetSoundCloudMedia(url string) (*ExternalMedia, error) {
+// 	var err error
+// 	_, body, errs := gorequest.New().Get("https://api.soundcloud.com/resolve.json?url=" + url + "&client_id=" + APIKeys.SoundCloud.ID).EndBytes()
+
+// 	if len(errs) > 0 {
+// 		return nil, errs[0]
+// 	}
+
+// 	var soundcloud SoundCloudTrack
+// 	err = json.Unmarshal(body, &soundcloud)
+
+// 	if err != nil {
+// 		return nil, err
+// 	}
+
+// 	if soundcloud.ID == 0 {
+// 		return nil, errors.New("Invalid Soundcloud response as the ID is not valid")
+// 	}
+
+// 	soundCloudID := strconv.Itoa(soundcloud.ID)
+
+// 	return &ExternalMedia{
+// 		Service:   "SoundCloud",
+// 		ServiceID: soundCloudID,
+// 	}, nil
+// }
