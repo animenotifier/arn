b3de387dcd82730be9c6996013dcfe8aafeb022c
Eduard Urbach <e.urbach@gmail.com>
2018-04-14 21:36:13 +0200

    Added initial AMV struct

---
+++ AMV.go
@@ -0,0 +1,14 @@
+package arn
+
+// AMV is an anime music video.
+type AMV struct {
+	ID          string   `json:"id"`
+	MainAnimeID string   `json:"mainAnimeId" editable:"true"`
+	AnimeIDs    []string `json:"animeIds" editable:"true"`
+	Tags        []string `json:"tags" editable:"true"`
+	IsDraft     bool     `json:"isDraft" editable:"true"`
+
+	HasCreator
+	HasEditor
+	HasLikes
+}
