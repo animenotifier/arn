3f063ed3832ad873b037e97e6f077b68e8b196ef
Eduard Urbach <e.urbach@gmail.com>
2018-04-15 10:36:39 +0200

    Improved AMV editing

--- AMV.go
+++ AMV.go
@@ -2,14 +2,17 @@ package arn
 
 import (
 	"errors"
+	"io/ioutil"
 	"os"
 	"path"
+
+	"github.com/aerogo/nano"
 )
 
 // AMV is an anime music video.
 type AMV struct {
 	ID            string   `json:"id"`
-	File          string   `json:"file" editable:"true" type:"upload" filetype:"video" endpoint:"/api/upload/amv"`
+	File          string   `json:"file" editable:"true" type:"upload" filetype:"video" endpoint:"/api/upload/amv/:id/file"`
 	Title         AMVTitle `json:"title" editable:"true"`
 	MainAnimeID   string   `json:"mainAnimeId" editable:"true"`
 	ExtraAnimeIDs []string `json:"extraAnimeIds" editable:"true"`
@@ -26,6 +29,42 @@ func (amv *AMV) Link() string {
 	return "/amv/" + amv.ID
 }
 
+// SetVideoBytes sets the bytes for the video file.
+func (amv *AMV) SetVideoBytes(data []byte) error {
+	fileName := amv.ID + ".webm"
+	filePath := path.Join(Root, "videos", "amvs", fileName)
+	err := ioutil.WriteFile(filePath, data, 0644)
+
+	if err != nil {
+		return err
+	}
+
+	amv.File = fileName
+	return nil
+}
+
+// MainAnime returns main anime for the AMV, if available.
+func (amv *AMV) MainAnime() *Anime {
+	mainAnime, _ := GetAnime(amv.MainAnimeID)
+	return mainAnime
+}
+
+// ExtraAnime returns main anime for the AMV, if available.
+func (amv *AMV) ExtraAnime() []*Anime {
+	objects := DB.GetMany("Anime", amv.ExtraAnimeIDs)
+	animes := []*Anime{}
+
+	for _, obj := range objects {
+		if obj == nil {
+			continue
+		}
+
+		animes = append(animes, obj.(*Anime))
+	}
+
+	return animes
+}
+
 // Publish ...
 func (amv *AMV) Publish() error {
 	// No draft
@@ -33,9 +72,23 @@ func (amv *AMV) Publish() error {
 		return errors.New("Not a draft")
 	}
 
+	// No title
+	if amv.Title.String() == "" {
+		return errors.New("AMV doesn't have a title")
+	}
+
 	// No anime found
 	if amv.MainAnimeID == "" && len(amv.ExtraAnimeIDs) == 0 {
 		return errors.New("Need to specify at least one anime")
+	}
+
+	// No file uploaded
+	if amv.File == "" {
+		return errors.New("You need to upload a WebM file for this AMV")
+	}
+
+	if _, err := os.Stat(path.Join(Root, "videos", "amvs", amv.File)); os.IsNotExist(err) {
+		return errors.New("You need to upload a WebM file for this AMV")
 	}
 
 	draftIndex, err := GetDraftIndex(amv.CreatedBy)
@@ -46,10 +99,6 @@ func (amv *AMV) Publish() error {
 
 	if draftIndex.AMVID == "" {
 		return errors.New("AMV draft doesn't exist in the user draft index")
-	}
-
-	if _, err := os.Stat(path.Join(Root, "videos", "amvs", amv.ID+".webm")); os.IsNotExist(err) {
-		return errors.New("You need to upload a WebM file for this AMV")
 	}
 
 	amv.IsDraft = false
@@ -108,4 +157,49 @@ func GetAMV(id string) (*AMV, error) {
 	}
 
 	return obj.(*AMV), nil
+}
+
+// StreamAMVs returns a stream of all AMVs.
+func StreamAMVs() chan *AMV {
+	channel := make(chan *AMV, nano.ChannelBufferSize)
+
+	go func() {
+		for obj := range DB.All("AMV") {
+			channel <- obj.(*AMV)
+		}
+
+		close(channel)
+	}()
+
+	return channel
+}
+
+// AllAMVs returns a slice of all AMVs.
+func AllAMVs() []*AMV {
+	var all []*AMV
+
+	stream := StreamAMVs()
+
+	for obj := range stream {
+		all = append(all, obj)
+	}
+
+	return all
+}
+
+// FilterAMVs filters all AMVs by a custom function.
+func FilterAMVs(filter func(*AMV) bool) []*AMV {
+	var filtered []*AMV
+
+	channel := DB.All("AMV")
+
+	for obj := range channel {
+		realObject := obj.(*AMV)
+
+		if filter(realObject) {
+			filtered = append(filtered, realObject)
+		}
+	}
+
+	return filtered
 }
