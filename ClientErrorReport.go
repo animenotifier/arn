9808626078aeec752cabee0adc457fbb6571ee4b
Eduard Urbach <e.urbach@gmail.com>
2018-04-17 18:21:25 +0200

    Added iteration for client error reports

--- ClientErrorReport.go
+++ ClientErrorReport.go
@@ -1,5 +1,7 @@
 package arn
 
+import "github.com/aerogo/nano"
+
 // ClientErrorReport saves JavaScript errors that happen in web clients like browsers.
 type ClientErrorReport struct {
 	ID           string `json:"id"`
@@ -10,4 +12,32 @@ type ClientErrorReport struct {
 	ColumnNumber int    `json:"columnNumber"`
 
 	HasCreator
+}
+
+// StreamClientErrorReports returns a stream of all characters.
+func StreamClientErrorReports() chan *ClientErrorReport {
+	channel := make(chan *ClientErrorReport, nano.ChannelBufferSize)
+
+	go func() {
+		for obj := range DB.All("ClientErrorReport") {
+			channel <- obj.(*ClientErrorReport)
+		}
+
+		close(channel)
+	}()
+
+	return channel
+}
+
+// AllClientErrorReports returns a slice of all characters.
+func AllClientErrorReports() []*ClientErrorReport {
+	var all []*ClientErrorReport
+
+	stream := StreamClientErrorReports()
+
+	for obj := range stream {
+		all = append(all, obj)
+	}
+
+	return all
 }
