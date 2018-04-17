8d1b3984682c64e354c07d235fb3f617b7d6c209
Eduard Urbach <e.urbach@gmail.com>
2018-04-17 12:28:06 +0200

    Fixed IDList.Append()

--- IDList.go
+++ IDList.go
@@ -14,7 +14,7 @@ func GetIDList(id string) (IDList, error) {
 	return *obj.(*IDList), nil
 }
 
-// Append appends the given ID to the end of the list.
-func (idList IDList) Append(id string) {
-	idList = append(idList, id)
+// Append appends the given ID to the end of the list and returns the new IDList.
+func (idList IDList) Append(id string) IDList {
+	return append(idList, id)
 }
