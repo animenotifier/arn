65cab464e64f493ef1968e399d0ff6b6a37b3db6
Eduard Urbach <e.urbach@gmail.com>
2018-04-23 10:31:53 +0200

    Added CharacterName.ByUser()

--- CharacterName.go
+++ CharacterName.go
@@ -7,3 +7,29 @@ type CharacterName struct {
 	Japanese  string   `json:"japanese" editable:"true"`
 	Synonyms  []string `json:"synonyms" editable:"true"`
 }
+
+// ByUser returns the preferred name for the given user.
+func (name *CharacterName) ByUser(user *User) string {
+	if user == nil {
+		return name.Canonical
+	}
+
+	switch user.Settings().TitleLanguage {
+	case "canonical", "romaji":
+		return name.Canonical
+	case "english":
+		if name.English == "" {
+			return name.Canonical
+		}
+
+		return name.English
+	case "japanese":
+		if name.Japanese == "" {
+			return name.Canonical
+		}
+
+		return name.Japanese
+	default:
+		panic("Invalid name language")
+	}
+}
