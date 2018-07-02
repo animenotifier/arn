3e77d1dd744cd03bff7ee0ecc1507fc5978a288b
Eduard Urbach <e.urbach@gmail.com>
2018-07-02 15:23:59 +0900

    Removed www from youtube embed URL

--- ExternalMedia.go
+++ ExternalMedia.go
@@ -15,13 +15,13 @@ type ExternalMedia struct {
 	ServiceID string `json:"serviceId" editable:"true"`
 }
 
-// EmbedLink ...
+// EmbedLink returns the embed link used in iframes for the given media.
 func (media *ExternalMedia) EmbedLink() string {
 	switch media.Service {
 	case "SoundCloud":
 		return "//w.soundcloud.com/player/?url=https://api.soundcloud.com/tracks/" + media.ServiceID + "?auto_play=false&hide_related=true&show_comments=false&show_user=false&show_reposts=false&visual=true"
 	case "Youtube":
-		return "//www.youtube.com/embed/" + media.ServiceID + "?showinfo=0"
+		return "//youtube.com/embed/" + media.ServiceID + "?showinfo=0"
 	case "DailyMotion":
 		return "//www.dailymotion.com/embed/video/" + media.ServiceID
 	case "NicoVideo":
