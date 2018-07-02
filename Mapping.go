aa06ab2fd631b4002bd1a26b42f3a4573857d534
Eduard Urbach <e.urbach@gmail.com>
2018-07-02 17:41:08 +0900

    Added anilist/studio mapping

--- Mapping.go
+++ Mapping.go
@@ -5,6 +5,7 @@ func init() {
 	DataLists["mapping-services"] = []*Option{
 		&Option{"anidb/anime", "anidb/anime"},
 		&Option{"anilist/anime", "anilist/anime"},
+		&Option{"anilist/studio", "anilist/studio"},
 		&Option{"ann/company", "ann/company"},
 		&Option{"imdb/anime", "imdb/anime"},
 		&Option{"kitsu/anime", "kitsu/anime"},
