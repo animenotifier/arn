8de33f74faecd73fb9aed5f2d327de627c58c746
Eduard Urbach <e.urbach@gmail.com>
2018-11-01 17:16:43 +0900

    Fixed thread text

--- Thread.go
+++ Thread.go
@@ -10,7 +10,6 @@ import (
 // Thread is a forum thread.
 type Thread struct {
 	Title  string   `json:"title" editable:"true"`
-	Text   string   `json:"text" editable:"true" type:"textarea"`
 	Sticky int      `json:"sticky" editable:"true"`
 	Tags   []string `json:"tags" editable:"true"`
 	Edited string   `json:"edited"`
