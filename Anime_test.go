ffbd0e2d7e3af1820d52fa322ff7f117ba120327
Eduard Urbach <e.urbach@gmail.com>
2019-03-11 21:30:56 +0900

    Improved Anime type tests

--- Anime_test.go
+++ Anime_test.go
@@ -7,7 +7,28 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestStreamAnime(t *testing.T) {
+func TestNewAnime(t *testing.T) {
+	anime := arn.NewAnime()
+	assert.NotNil(t, anime)
+	assert.NotEmpty(t, anime.ID)
+	assert.NotEmpty(t, anime.Created)
+}
+
+func TestGetAnime(t *testing.T) {
+	// Existing anime
+	anime, err := arn.GetAnime("74y2cFiiR")
+	assert.NoError(t, err)
+	assert.NotNil(t, anime)
+	assert.NotEmpty(t, anime.ID)
+	assert.NotEmpty(t, anime.Title.Canonical)
+
+	// Not existing anime
+	anime, err = arn.GetAnime("does not exist")
+	assert.Error(t, err)
+	assert.Nil(t, anime)
+}
+
+func TestAllAnime(t *testing.T) {
 	validAnimeStatus := []string{
 		"finished",
 		"current",
@@ -15,13 +36,36 @@ func TestStreamAnime(t *testing.T) {
 		"tba",
 	}
 
-	for anime := range arn.StreamAnime() {
+	validAnimeType := []string{
+		"tv",
+		"movie",
+		"ova",
+		"ona",
+		"special",
+		"music",
+	}
+
+	allAnime := arn.AllAnime()
+
+	for _, anime := range allAnime {
 		assert.NotEmpty(t, anime.ID)
 		assert.Contains(t, validAnimeStatus, anime.Status)
+		assert.Contains(t, validAnimeType, anime.Type)
+		assert.Contains(t, validAnimeStatus, anime.CalculatedStatus())
+		assert.NotEmpty(t, anime.StatusHumanReadable())
+		assert.NotEmpty(t, anime.TypeHumanReadable())
 		assert.NotEmpty(t, anime.Link())
+		assert.NotEmpty(t, anime.EpisodeCountString())
 
 		anime.Episodes()
 		anime.Characters()
+		anime.StartDateTime()
+		anime.EndDateTime()
+		anime.HasImage()
 		anime.GetMapping("shoboi/anime")
+		anime.Studios()
+		anime.Producers()
+		anime.Licensors()
+		anime.Prequels()
 	}
 }
